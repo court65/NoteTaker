3599adcf2dad6d87f7b1e53589368220669b2d8d
Court Evans <court_re@yahoo.com>
2022-04-19 17:05:01 -0500

    added inport express and function for new note

--- Main/server.js
+++ Main/server.js
@@ -4,3 +4,34 @@ const path = require('path');
 
 const express = require('express');
 const app = express();
+
+const allNotes = require('./db/db.json');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
